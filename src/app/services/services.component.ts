ad93d1bff277a7d3df52e32eb6c1530f28e527a2
Maley Venkatesh <venkateshmaley1@gmail.com>
2025-02-15 21:32:36 +0530

    Services page seo update

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-services',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+  constructor(private meta: Meta, private title: Title) {
+    this.title.setTitle('SreeVia AI Services | Transforming Industries with AI');
 
+    this.meta.updateTag({ name: 'description', content: 'Explore SreeVia AI\'s services, including AI Academy, consulting, and custom solutions for healthcare, education, and more. Contact +919848022338 today.' });
+    this.meta.updateTag({ name: 'keywords', content: 'AI services, AI academy, AI consulting, custom AI solutions, healthcare AI, education AI' });
+    this.meta.updateTag({ name: 'robots', content: 'index, follow' });
+  }
 }
