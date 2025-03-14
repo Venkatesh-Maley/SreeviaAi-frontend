cc84dfeb0ad204b2ea6340e237ad45c22486e893
Maley Venkatesh <venkateshmaley1@gmail.com>
2025-03-14 23:51:15 +0530

    cur

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+// document.addEventListener('mousemove', (e) => {
+//   const cursor = document.getElementById('custom-cursor');
+//   if (cursor) {
+//     cursor.style.left = `${e.clientX}px`;
+//     cursor.style.top = `${e.clientY}px`;
+//   }
+// });
