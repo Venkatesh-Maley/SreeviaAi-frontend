235ee65544d8d504d438e2ee37d71de1ea43aff7
Maley Venkatesh <venkateshmaley1@gmail.com>
2025-03-14 22:55:29 +0530

    seo up

--- src/app/seo-service/seo.service.ts
+++ src/app/seo-service/seo.service.ts
@@ -1,21 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SeoService {
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
 
   updateCanonicalUrl(url: string): void {
-    const existingLink: HTMLLinkElement | null = document.querySelector("link[rel='canonical']");
-    
-    if (existingLink) {
-      existingLink.href = url;
-    } else {
-      const link: HTMLLinkElement = document.createElement('link');
-      link.setAttribute('rel', 'canonical');
-      link.setAttribute('href', url);
-      document.head.appendChild(link);
+    if (isPlatformBrowser(this.platformId)) {
+      const existingLink: HTMLLinkElement | null = document.querySelector("link[rel='canonical']");
+
+      if (existingLink) {
+        existingLink.href = url;
+      } else {
+        const link: HTMLLinkElement = document.createElement('link');
+        link.setAttribute('rel', 'canonical');
+        link.setAttribute('href', url);
+        document.head.appendChild(link);
+      }
     }
   }
 }
