7b10038b212100569b7618fb09926ee99814a73f
Maley Venkatesh <venkateshmaley1@gmail.com>
2025-08-11 12:59:24 +0530

    dte changes

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -23,9 +23,9 @@ export class NewsComponent implements OnInit {
     this.seoService.updateCanonicalUrl('https://www.sreeviaai.com/news');
   }
    posts = [
+    { title: 'AI meeting conducted by the IESA.', description: 'SreeVia AI CEO attend an AI meeting conducted by the India Electronics and Semiconductor Association (IESA).', date: 'November 7, 2023', image: 'image3.jpg' },
+    { title: 'SreeVia Ai recognized by Global C-Suite Community platform', description: 'SreeVia AI Technologies has been recognized with a prestigious award for AI Innovation.', date: 'December 7, 2024', image: 'image1.png' },
     { title: 'Strengthening AI through Strategic Partnerships', description: 'Sreevia AI CEO attended the APTA Katalyst Global Business Conference 2025 held in Hyderabad.', date: 'January 5, 2025', image: 'image2.png' },
-    { title: 'SreeVia Ai recognized by Global C-Suite Community platform', description: 'SreeVia AI Technologies has been recognized with a prestigious award for AI Innovation.', date: 'December 7, 2025', image: 'image1.png' },
-    { title: 'AI meeting conducted by the IESA.', description: 'SreeVia AI CEO attend an AI meeting conducted by the India Electronics and Semiconductor Association (IESA).', date: 'March 15, 2025', image: 'image3.jpg' },
     // { title: 'AI-driven Healthcare Solutions', description: 'Sreevia AI is pioneering solutions in healthcare, transforming patient diagnosis and treatment using AI.', date: 'March 12, 2025', image: 'image3.png' },
     // { title: 'AI in Autonomous Vehicles', description: 'The future of transportation is here, and Sreevia AI is at the forefront of autonomous vehicle technology.', date: 'March 10, 2025', image: 'image3.png' },
     // { title: 'Breaking Boundaries in Natural Language Processing', description: 'Sreevia AI\'s breakthrough in NLP is making waves in industries ranging from healthcare to finance.', date: 'March 8, 2025', image: 'image3.png' },
